7edc1ed478fda862d6a5cbe818452821818b4478
reaz haq <reaz@bluebonnetsoftware.com>
2019-02-14 21:26:50 -0600

    Integrated tests

---
+++ NetCoreApiOneTests/Controllers/BlahBlahControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Http;
+using NetCoreApiOne;
+using Xunit;
+
+namespace NetCoreApiOneTests.Controllers
+{
+    public class BlahBlahControllerTests : IClassFixture<TestFixture<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public BlahBlahControllerTests(TestFixture<Startup> fixture)
+        {
+            _client = fixture.Client;
+        }
+
+        [Fact]
+        public void BlahTest()
+        {
+            var request = new HttpRequestMessage(new HttpMethod("GET"), "/api/BlahBlah");
+
+            var response = _client.SendAsync(request).Result;
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ NetCoreApiOneTests/TestFixture.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using Lamar.Microsoft.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+namespace NetCoreApiOneTests
+{
+    public class TestFixture<TStartup> : IDisposable where TStartup : class
+    {
+        private readonly TestServer _server;
+
+        public TestFixture()
+        {
+            var builder = new WebHostBuilder()
+                .UseLamar()
+                .UseStartup<TStartup>();
+            _server = new TestServer(builder);
+
+            Client = _server.CreateClient();
+            Client.BaseAddress = new Uri("http://localhost:5000");
+        }
+
+        public HttpClient Client { get; }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            _server.Dispose();
+        }
+    }
+}
